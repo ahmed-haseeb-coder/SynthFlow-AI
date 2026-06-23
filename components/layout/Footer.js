f2bf4b624208a08e0a049e7931006873ee96bc4f
ahmed-haseeb-coder <ahmedhasee40@gmail.com>
2026-06-23 18:24:43 +0500

    new profolio project

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,4 +1,4 @@
-import Container from "@/components/ui/Container";
+ import Container from "@/components/ui/Container";
 import { SITE } from "@/lib/constants";
 
 export default function Footer() {
